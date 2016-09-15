2b1f14c8288f896424ee416179343187d042b0e7
Frazier Mork <fraziermork@gmail.com>
2016-09-14 20:26:35 -0700

    added removal task to start of initialize

--- backend/initialize/initialize.js
+++ backend/initialize/initialize.js
@@ -3,10 +3,14 @@
 const debug           = require('debug')('fm:initialize');
 const mongooseManager = require('../lib/mongoose-manager');
 const seedDatabase    = require('./lib/seed-database');
+const Article         = require('../resources/article/article-model');
 
 debug('about to seed database');
 
 mongooseManager.startConnectionToDatabase()
+ .then(() => {
+   Article.find({}).remove().exec();
+ })
  .then(() => {
    return seedDatabase.seedDatabaseWithArticles(`${__dirname}/../../data/articles`);
  })
